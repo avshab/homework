b4c9f3c71fb5cf0601db7155f67c8cba09deb138
Anna Shabaeva <av.shabaeva@severmed.com>
2019-12-24 16:01:10 +0300

    add

--- app/src/main/java/ru/skillbranch/kotlinexample/extentions/String.kt
+++
@@ -1,6 +0,0 @@
-package ru.skillbranch.kotlinexample.extentions
-
-fun String.isValidPhone() =
-    if (trimPhone().startsWith("+") && trimPhone().length == 12) true
-    else false
-fun String.trimPhone() = this.replace("[^+\\d]".toRegex(), replacement = "")
---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/SubmenuBehavior.kt
@@ -0,0 +1,44 @@
+package ru.skillbranch.skillarticles.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import ru.skillbranch.skillarticles.extensions.dpToPx
+import kotlin.math.hypot
+import kotlin.math.max
+import kotlin.math.min
+
+
+class SubmenuBehavior<V : View>(context: Context, attrs: AttributeSet) :
+    CoordinatorLayout.Behavior<V>(context, attrs) {
+    private var centerX: Float = context.dpToPx(200)
+    private var centerY: Float = context.dpToPx(96)
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        directTargetChild: View,
+        target: View,
+        axes: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        if (child.visibility == View.VISIBLE) {
+            child.translationX = max(0f, min(hypot(centerX, centerY), child.translationX + dy))
+            child.translationY = max(0f, min(hypot(centerX, centerY), child.translationY + dy))
+        }
+    }
+}
